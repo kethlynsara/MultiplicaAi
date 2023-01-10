dfe1929329e0e3d6cb3d41a3131d411423f61446
Kethlyn <kethlynsaraa@gmail.com>
2023-01-10 00:56:03 -0300

    first commit

---
+++ src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int number = 7;
+        System.out.println("Tabela de multiplicação de " + number);
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(number + " x " + i + " = " + number * i);
+        }
+    }   
+}
